5251cdf0b0c7d5f44d8c864905ad7ec3ebbaecb4
Giggly Bits <71092239+greaselovely@users.noreply.github.com>
2021-08-08 20:56:23 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import ldap3
+from ldap3 import ALL
+
+server = ldap3.Server('1.2.3.4', get_info=ALL)
+password = 'ThxforAllTheFish!'
+baseDN = 'dc=ss-syn-01,dc=fennon,dc=com'
+bindDN = 'uid=root,cn=users,' + baseDN
+groupDN = 'cn=groups,' + baseDN
+objectclassTypes = ['top', 'person', 'posixAccount', 'organizationalPerson', '*']
+
+conn = ldap3.Connection(server, bindDN, password, auto_bind=True)
+
+#server.schema
+
+for type in objectclassTypes:
+    try:
+        conn.search(baseDN, '(&(objectclass=' + type + '))')
+        entries = conn.entries
+    except ldap3.core.exceptions.LDAPInvalidFilterError as e:
+        print(e, conn.search)
+
+print(entries[::-1])
